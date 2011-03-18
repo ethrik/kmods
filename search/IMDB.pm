83a6ac9847e4ad52928e4aaf18a2af09b8811b61
Alyx <alyx@sporksmoo.net>
2011-03-17 21:17:32 -0500

    Silly array.

--- search/IMDB.pm
+++ search/IMDB.pm
@@ -15,8 +15,7 @@ $keldair->command_bind(IMDB => sub {
         my ( $chan, $nick, @parv ) = @_;
         my $query = join(' ', @parv);
         my $imdb = IMDB::Film->new( crit => $query, user_agent => "Keldair/$Keldair::VERSION", timeout => 2, debug => 0, cache => 0);
-        my @data = ( $imdb->title, $imdb->year, $imdb->plot);
-        if ( $imdb->status ) { $keldair->msg($chan, "Title: $data[0], Year: $data[1], Summary: $data[2]"); }
+        if ( $imdb->status ) { $keldair->msg($chan, 'Title: %s, Year: %s, Summary: %s', $imdb->title, $imdb->year, $imdb->plot ); }
         else { $keldair->msg($chan, "Could not find $query"); }
     }
 );
