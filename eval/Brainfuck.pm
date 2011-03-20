2132d4ea7868641580f4a0bc8b3624ba83260fb2
Alyx <alyx@sporksmoo.net>
2011-03-20 01:20:12 -0500

    Aded a brainfuck interpreter. Why? Why not.

---
+++ eval/Brainfuck.pm
@@ -0,0 +1,26 @@
+# Brainfuck.pm - Brainfuck interpreter for Keldair
+# Copyright 2011 Alexandria M. Wolcott <alyx@sporksmoo.net>
+# Released under the same terms as Perl itself.
+
+# modreq: Language::BF
+
+use strict;
+use warnings;
+package Keldair::Module::Brainfuck;
+
+use Language::BF;
+use Keldair;
+
+$keldair->help_add(BF => 'Evaluates a snippet of Brainfuck code.');
+$keldair->syntax_add(BF => 'BF <code>');
+
+$keldair->command_bind(BF => 
+    sub {
+        my ( $chan, $nick, @msg ) = @_;
+        my $bf = Language::BF->new($msg[0]);
+        $bf->run;
+        $keldair->msg($chan, $bf->output);
+    }
+);
+
+1;
