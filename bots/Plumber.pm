6a3d8ee17eaa9885572441061754bfcfed2c8f66
Lee T. Starnes <lstarnes1024@gmail.com>
2011-04-03 20:22:49 -0400

    Add Plumber, a rather simple module for scanning messages from one channel then delivering them to another based on regexes

---
+++ bots/Plumber.pm
@@ -0,0 +1,26 @@
+# Plumber.pm - Keldair module for routing messages from one channel to another.
+# Copyright 2011 Lee T. Starnes <lstarnes1024@gmail.com>
+# Released under the same terms as Perl itself.
+
+use strict;
+use warnings;
+
+package Keldair::Module::Plumber;
+
+use Keldair;
+use Keldair::State;
+
+$keldair->hook_add(OnMessage => \&handle_message);
+
+sub handle_message {
+    my ( $network, $channel, $origin, $message ) = @_;
+    if (my $filters = $keldair->config("plumber/$network/$channel")) {
+	foreach my $filter (@{ $filters }) {
+            my $regex = qr/$filter->{regex}/;
+            if ($message =~ /$regex/) {
+		$keldair->msg($filter->{to}->{network}, $keldair->find_chan($filter->{to}->{channel}), $message);
+            }
+        }
+    }
+}
+
