6f4be271e41f1ddcb7d14cdce8d053b9fcbb6b4a
Alyx <alyx@sporksmoo.net>
2011-04-11 23:15:06 -0500

    Added HTTP authentication. This allows Keldair to authenticate users via standard HTTP login authentication (I was bored, and there must be someone, somewhere, that would find this useful, somehow).

---
+++ authen/Http.pm
@@ -0,0 +1,30 @@
+# HTTP.pm - HTTP authentication for Keldair
+# Copyright 2011 Alexandria M. Wolcott <alyx@sporksmoo.net>
+# Released under the same terms at Perl itself.
+
+# modreq: Authen::Simple::HTTP
+
+use strict;
+use warnings;
+package Keldair::Module::Authen::Http;
+
+use base 'Exporter';
+use Keldair;
+use Authen::Simple::HTTP;
+
+our @EXPORT = qw( &check );
+
+my $auth = Authen::Simple::HTTP->new( url => $keldair->conf->get('authen/url') )
+    or warn "Authen::Simple::Atheme startup failure: $!" and return;
+
+sub check {
+    my ( $username, $password ) = @_;
+    if ( $auth->check( $username, $password ) ) {
+            return 1;
+    }
+    else {
+        return;
+    }
+}
+
+1;
