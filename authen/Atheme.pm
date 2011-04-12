7a681981ef7ba41e527e481c5ec3cc05764c7171
Alyx <alyx@sporksmoo.net>
2011-04-11 22:12:35 -0500

    Added the Atheme authentication module. This interfaces with Authen::Simple::Atheme to provide authentication to Keldair using Atheme details, confirmed over XMLRPC. <3 Atheme!

---
+++ authen/Atheme.pm
@@ -0,0 +1,28 @@
+# Atheme.pm - Atheme authentication integration for Keldair
+# Copyright 2011 Alexandria M. Wolcott <alyx@sporksmoo.net>
+# Released under the same terms at Perl itself.
+
+use strict;
+use warnings;
+package Keldair::Module::Authen::Atheme;
+
+use base 'Exporter';
+use Keldair;
+use Authen::Simple::Atheme;
+
+our @EXPORT = qw( &check );
+
+my $auth = Authen::Simple::Atheme->new( host => $keldair->conf->get('authen/host'), port => $keldair->conf->get('authen/port') )
+    or warn "Authen::Simple::Atheme startup failure: $!" and return;
+
+sub check {
+    my ( $username, $password ) = @_;
+    if ( $auth->check( $username, $password ) ) {
+            return 1;
+    }
+    else {
+        return;
+    }
+}
+
+1;
