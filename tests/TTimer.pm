7aa18984f2fd00b94ab2d176304f296258dcd805
Matthew Barksdale <matthew@matt-tech.info>
2011-04-21 22:45:13 -0400

    Added a module to test timers

---
+++ tests/TTimer.pm
@@ -0,0 +1,20 @@
+# TTimer.pm - Keldair module for testing timers.
+# Copyright 2011 Matthew Barksdale <matthew@matt-tech.info>
+# Licensed under the Artistic License
+
+package Keldair::Module::Tests::TTimer;
+
+use strict;
+use warnings;
+use Keldair;
+
+$keldair->help_add(TTIMER => 'Tests timers');
+$keldair->syntax_add(TTIMER => 'TTIMER');
+
+$keldair->command_bind(
+    TTIMER => sub {
+        my ( $network, $chan, $nick, $string ) = @_;
+	$keldair->msg( $network, $chan, "Sending DICKS after 10 seconds.");
+	$timer->after(10, sub { $keldair->msg( $network, $chan, "DICKS");  });
+    }
+);
