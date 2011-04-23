39376f9446d4e775147962a7a356b632db130b5d
Matthew Barksdale <matthew@matt-tech.info>
2011-04-23 17:42:39 -0400

    Added a test module to test hooks

---
+++ tests/THooks.pm
@@ -0,0 +1,26 @@
+# THooks.pm - Keldair module for testing hooks.
+# Copyright 2011 Matthew Barksdale <matthew@matt-tech.info>
+# Licensed under the Artistic License
+
+package Keldair::Module::Tests::THooks;
+
+use strict;
+use warnings;
+use Keldair;
+
+
+$keldair->help_add(THOOKS => 'Tests hooks');
+$keldair->syntax_add(THOOKS => 'THOOKS <test string>');
+
+$keldair->command_bind(
+    THOOKS => sub {
+        my ( $network, $chan, $nick, $string ) = @_;
+	    $keldair->hook_run(OnJoin => $network, $nick, $chan);
+        $keldair->hook_run(OnKick => $network, $nick, $chan);
+        $keldair->hook_run(OnJoin => $network, $nick, $chan);
+        $keldair->hook_run(OnPart => $network, $nick, $chan);
+        $keldair->hook_run(OnNotice => $network, $nick, $chan, $string);
+        $keldair->hook_run(OnMessage => $network, $chan, $nick, $string);
+        $keldair->hook_run(OnRehash);
+    }
+);
