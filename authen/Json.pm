ce1450a8decae363ab5f3a63d080c0e23859928c
Lee T. Starnes <lstarnes1024@gmail.com>
2011-04-23 02:22:54 -0400

    Add registration to Authen::Json

--- authen/Json.pm
+++ authen/Json.pm
@@ -20,6 +20,32 @@ $conf = $ENV{HOME}."/.keldair/accounts.json" if $Bin eq "/usr/bin";
 
 my $db = Config::JSON->new($conf);
 
+$keldair->help_add('REGISTER' => "Register an account");
+$keldair->syntax_add('REGISTER' => "REGISTER <username> <password>");
+
+$keldair->command_bind('REGISTER' => sub {
+        my ( $network, $chan, $origin, $msg) = @_;
+        my @parv = split(' ', $msg);
+        if ( $#parv == 1 ) {
+            $username = lc $parv[0];
+            $password = $parv[1];
+        }
+        else {
+            $username = lc $origin->nick;
+            $password = $parv[0];
+        }
+	if defined($db->get(lc($username))) {
+	    $keldair->msg($network, $origin, "$username is already registered.");
+        }
+        else {
+            my $sha = new Digest::SHA2;
+            $sha->add($password);
+            $db->set(lc $username, $sha->hexdigest);
+            $keldair->msg($network, $origin, "You are now registered as $username.");
+        }
+    }
+);
+
 sub check {
     my ( $username, $password ) = @_;
     return if !defined($db->get($username));
